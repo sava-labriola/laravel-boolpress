9f0bdce900b7a1ac7bcd450edafd3e446098882f
sava-labriola <labriolasava00@gmail.com>
2020-07-13 20:16:33 +0200

    utente e admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1>Hai fatto il login nella dashboard</h1>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::prefix('/admin')->namespace('Admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('/posts', 'PostController');
+});
